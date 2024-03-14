45730f8c57d4bfd5c5f2afd037350d1f5e0058ab
Piratelle <eratelle@berkeley.edu>
2024-03-14 05:24:18 -0700

    Initial networking steps & README update

---
+++ Assets/Scripts/ConnectionApprovalHandler.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using Unity.Netcode;
+
+/// <summary>
+/// Class <c>ConnectionApprovalHandler</c> provides control logic to limit new client connections.
+/// This class heavily references code from <see href="https://www.youtube.com/watch?v=swIM2z6Foxk"/>.
+/// </summary>
+public class ConnectionApprovalHandler : MonoBehaviour
+{
+    private const int MaxPlayers = 4;
+
+    /// <summary>
+    /// Method <c>Start</c> subscribes the handler to ApprovalCheck events.
+    /// </summary>
+    private void Start()
+    {
+        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
+    }
+
+    /// <summary>
+    /// Method <c>ApprovalCheck</c> approves client connection only if there are available client slots.
+    /// </summary>
+    /// <param name="request">the client connection request.</param>
+    /// <param name="response">the handler's response to the request.</param>
+    private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
+    {
+        Debug.Log("Validating connection...");
+        response.Approved = true;
+        response.CreatePlayerObject = true;
+        response.PlayerPrefabHash = null;
+        if (NetworkManager.Singleton.ConnectedClients.Count >= MaxPlayers)
+        {
+            response.Approved = false;
+            response.Reason = "Player limit reached.";
+        }
+        response.Pending = false;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Unpac_d
+# Unpac'd
+A game for 1 - 4 players not-so-loosely inspired by the iconic Pac-Man!
+
+This project was developed in **Unity** using **Netcode for GameObjects** as a part of the **Advanced Game Design** curriculum at SDSU for the Spring 2024 semester.
+
+## External Resources
+
+### Image Assets
+Using the **[GameDev Market RPG Humble Bundle](https://www.humblebundle.com/software/rpg-game-dev-bundle)** ([Licence](https://www.gamedevmarket.net/terms-conditions/#pro-licence)):
+* [2D Hand Painted - Dungeon Tileset](https://www.gamedevmarket.net/asset/2d-hand-painted-dungeon-tileset-3011)
+* [7Soul's RPG Graphics - UI Pack](https://www.gamedevmarket.net/asset/7souls-rpg-graphics-pack-2-ui-1208)
+* [Monsters Time Fantasy RPG Sprite Pack](https://www.gamedevmarket.net/asset/monsters-time-fantasy-rpg-sprite-pack-4391)
+
+
+### References
+Many helpful resources were referenced in the making of this project, including:
+* [2D Animation Tutorial by Brackeys](https://www.youtube.com/watch?v=hkaysu1Z-N8)
+* [Unity Sprite Slicing Tutorial by ShootingDux](https://www.youtube.com/watch?v=_gDSfZ01GVE)
+* [Comprehensive Multiplayer Tutorial by samyam](https://www.youtube.com/watch?v=swIM2z6Foxk)
+* [Pacman in Unity Tutorial by Zigurous](https://www.youtube.com/watch?v=TKt_VlMn_aA)
