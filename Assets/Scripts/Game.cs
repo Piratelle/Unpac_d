5bc836c8e2d96734f062f33e123d785a4b2c14fd
Piratelle <eratelle@berkeley.edu>
2024-03-19 08:45:35 -0700

    More UI!

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -9,28 +9,11 @@ public class Game : MonoBehaviour
 {
     [SerializeField] private Transform pellets;
     [SerializeField] private GameObject[] playerPanels;
+    [SerializeField] private TMP_Text playersLabel;
 
     public bool IsGameOver { get; private set; } = true; // default to true for purposes of allowing new player connections
 
-    /// <summary>
-    /// Method <c>Start</c> initializes the object before starting a new game.
-    /// </summary>
-    private void Start()
-    {
-        NewGame();
-    }
-
-    /// <summary>
-    /// Method <c>Update</c> scans for user input that can affect game states.
-    /// </summary>
-    private void Update()
-    {
-        // potentially change this depending on UI!!
-        if (IsGameOver && Input.anyKeyDown)
-        {
-            NewGame();
-        }
-    }
+    private int playerCount = 0;
 
     /// <summary>
     /// Method <c>Activate</c> activates the UI for the provided player number.
@@ -38,22 +21,59 @@ public class Game : MonoBehaviour
     /// <param name="playerNum">the player whose UI should be activated.</param>
     public void Activate(int playerNum)
     {
+        SetPlayer(playerNum, true);
+    }
+
+    /// <summary>
+    /// Method <c>Deactivate</c> deactivates the UI for the provided player number.
+    /// </summary>
+    /// <param name="playerNum">the player whose UI should be deactivated.</param>
+    public void Deactivate(int playerNum)
+    {
+        SetPlayer(playerNum, false);
+    }
+
+    /// <summary>
+    /// Method <c>SetPlayer</c> handles the generic player activation update logic.
+    /// </summary>
+    /// <param name="playerNum"></param>
+    /// <param name="isActive">a boolean indicating if the player should be activated or deactivated.</param>
+    private void SetPlayer(int playerNum, bool isActive)
+    {
         if (playerNum <= playerPanels.Length && playerNum >= 0)
         {
             playerPanels[playerNum].SetActive(true);
+            playerCount += isActive ? 1 : -1;
+            playersLabel.text = playerCount.ToString() + "/4";
         }
     }
 
+    /// <summary>
+    /// Method <c>UpdatePlayerScore</c> handles the UI component of a score update.
+    /// </summary>
+    /// <param name="playerNum">the player whose score will be updated.</param>
+    /// <param name="score">the new score value.</param>
     public void UpdatePlayerScore(int playerNum, int score)
     {
         UpdatePlayerText(playerNum, "Score", score);
     }
 
+    /// <summary>
+    /// Method <c>UpdatePlayerBanked</c> handles the UI component of a banked score update.
+    /// </summary>
+    /// <param name="playerNum">the player whose banked score will be updated.</param>
+    /// <param name="banked">the new banked score value.</param>
     public void UpdatePlayerBanked(int playerNum, int banked)
     {
         UpdatePlayerText(playerNum, "Bank", banked);
     }
 
+    /// <summary>
+    /// Method <c>UpdatePlayerText</c> handles the generic player UI text update logic.
+    /// </summary>
+    /// <param name="playerNum">the player whose UI will be updated.</param>
+    /// <param name="name">the name of the text component that will be updated.</param>
+    /// <param name="value">the new value.</param>
     private void UpdatePlayerText(int playerNum, string name, int value)
     {
         if (playerNum <= playerPanels.Length && playerNum >= 0)
@@ -72,16 +92,16 @@ public class Game : MonoBehaviour
     /// <summary>
     /// Method <c>NewGame</c> resets all games states, whether level-based or game-based.
     /// </summary>
-    private void NewGame()
+    public void NewGame()
     {
         IsGameOver = false;
-        NewLevel(true);
+        NewLevel();
     }
 
     /// <summary>
     /// Method <c>NewLevel</c> resets all level-based game states.
     /// </summary>
-    private void NewLevel(bool isNewGame = false)
+    private void NewLevel()
     {
         // handle pellet re-population
         foreach (Transform pellet in pellets)
