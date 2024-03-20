4ffcfbf7aa267df7774cd1ab377e738d98b46fce
Piratelle <eratelle@berkeley.edu>
2024-03-19 19:40:55 -0700

    Fixing missed adds.

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+/// <summary>
+/// Class <c>AudioPlayer</c> controls audio via a Singleton to avoid noisy noise.
+/// This class references code from <see href="https://www.youtube.com/watch?v=3yuBOB3VrCk"/>.
+/// </summary>
+public class AudioPlayer : Singleton<AudioPlayer>
+{
+    [SerializeField] private AudioSource btnClkAudioSrc;
+    [SerializeField] private AudioSource btnHovAudioSrc;
+    [SerializeField] private AudioSource eatAudioSrc;
+    [SerializeField] private AudioSource enemyAudioSrc;
+    [SerializeField] private AudioSource gameOverAudioSrc;
+
+    public void PlayBtnClick()
+    {
+        btnClkAudioSrc.Play();
+    }
+
+    public void PlayBtnHover()
+    {
+        btnHovAudioSrc.Play();
+    }
+}
